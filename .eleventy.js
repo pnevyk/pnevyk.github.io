bede5b67fefa870f6201464eed2d652a0866c2a7
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2020-07-18 15:18:48 +0200

    Add 404 page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'));
     eleventyConfig.addPlugin(require('eleventy-plugin-excerpt'));
@@ -11,4 +13,21 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(require('eleventy-plugin-figures'));
 
     eleventyConfig.addPassthroughCopy('static');
+
+    // For development (`--serve`) purposes.
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function(err, bs) {
+                bs.addMiddleware('*', (req, res) => {
+                    const content404 = fs.readFileSync('_site/404.html');
+                    // Provides the 404 content without redirect.
+                    res.write(content404);
+                    // Add 404 http status code in request header.
+                    // res.writeHead(404, { "Content-Type": "text/html" });
+                    res.writeHead(404);
+                    res.end();
+                });
+            }
+        }
+    });
 };
