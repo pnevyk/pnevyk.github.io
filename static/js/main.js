a5c382e9ca1f37eb56f6b35aa159a061a8aa5c94
pnevyk <pnevyk@users.noreply.github.com>
2025-03-30 12:04:43 +0000

    deploy: pnevyk/blog@d3b81a84c44393ec30b5b37c2758f7080b7f5e16

--- static/js/main.js
+++ static/js/main.js
@@ -13,4 +13,30 @@
             progress.style.width = width + '%';
         }
     }
+
+    document.querySelectorAll('.tabs > .tabs-header > .tab-label:first-of-type').forEach((labelElement) => {
+        const tabId = labelElement.getAttribute("data-tab-id");
+        const tabContext = labelElement.parentElement.parentElement.getAttribute("data-tab-context");
+        selectTab(tabContext, tabId);
+    });
+
+    document.querySelectorAll('.tabs > .tabs-header > .tab-label').forEach((labelElement) => {
+        labelElement.addEventListener("click", () => {
+            const tabId = labelElement.getAttribute("data-tab-id");
+            const tabContext = labelElement.parentElement.parentElement.getAttribute("data-tab-context");
+            selectTab(tabContext, tabId);
+        });
+    });
+
+    function selectTab(tabContext, tabId) {
+        document.querySelectorAll(`.tabs[data-tab-context="${tabContext}"]`).forEach(contextElement => {
+            contextElement.querySelectorAll(".tab-label, .tab-content").forEach(element => {
+                element.classList.remove("active");
+            });
+
+            contextElement.querySelectorAll(`[data-tab-id="${tabId}"`).forEach(element => {
+                element.classList.add("active");
+            });
+        })
+    }
 })();
